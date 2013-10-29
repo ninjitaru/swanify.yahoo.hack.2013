7b8afb4f99df32da3e2b9aeec7ce283969dcf683
Jason Chang <ninjitaru@gmail.com>
2013-10-30 01:28:33 +0800

    use jquery for my search dom operation added price parsing for yahoo shops

--- plugins/chrome/swan_plugin/contentscript.js
+++ plugins/chrome/swan_plugin/contentscript.js
@@ -1,25 +1,30 @@
-images = document.getElementsByTagName('img');
-if(images)
+var srcList = [];
+
+jQuery("img").each(function(index,img){
+    src = img.src;
+    if(src.match(/l.yimg.com/) || src.match(/geo.yahoo.com/) || src.match(/api.uwh.ect.yahoo.com/))
+        return;
+    srcList.push(src);
+    console.log(src);
+});
+
+// Yahoo!奇摩購物中心 
+var price = jQuery('div.priceinfo span.price').text().replace(/\D/g, '');;
+if(!price)
 {
-    var srcList = [];
-
-    // console.log(jQuery("img").size());
-    // var regex = /tw.bid.yimg.com/;
-    for(var i = 0; i < images.length; i++) 
+    //Yahoo!奇摩超級商城 
+    price = jQuery('div[itemprop="offers"] span.price').text().replace(/\D/g, '');;
+    if(!price)
     {
-        if(images[i].src.match(/l.yimg.com/))
-        	continue;
-        if(images[i].src.match(/geo.yahoo.com/))
-        	continue;
-        if(images[i].src.match(/api.uwh.ect.yahoo.com/))
-        	continue;
-        srcList.push(images[i].src);
+        //Yahoo! 奇摩拍賣
+        price = jQuery('li.buy').text().replace(/\D/g, '');;
     }
-
-    alert(document.URL);
-    alert(document.title);
-    alert(images.length);
-    
-    jQuery.post( "http://pavo-prototype.herokuapp.com/item_lists/1/items", 
-        { url: document.URL, price : "100", images : srcList, title: document.title });
 }
+
+console.log(price);
+
+console.log(document.URL);
+console.log(document.title);
+
+jQuery.post( "http://pavo-prototype.herokuapp.com/item_lists/1/items?token=abc", 
+    { url: document.URL, price : price, images : srcList, title: document.title });
