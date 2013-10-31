b2914dd96301126ce3092a9ce66ac90eed66ec70
Jason Chang <ninjitaru@gmail.com>
2013-10-31 22:39:04 +0800

    show top item bar

--- plugins/chrome/swan_plugin/contentscript.js
+++ plugins/chrome/swan_plugin/contentscript.js
@@ -1,3 +1,9 @@
+var username = "ninji";
+
+
+deleteItemList(username);
+getItemListID(username);
+
 
 function setHeader(xhr) {
 
@@ -12,23 +18,31 @@ function printItemListSummary(itemList)
 }
 
 function showTopBar() {
-    var topbar;
-  // $('body').css({
-  //   'padding-top': '160px'
-  // });
-  topbar = $("<div id='topbar'></div>");
-  topbar.css({
-    'position': 'fixed',
-    'right': '0px',
-    'top': '-160px',
-    'z-index': 9999,
-    'width': '100%',
-    'height': '160px',
-    'background-color': 'blue'  // Confirm it shows up
-  });
-
-
-  $('body').append(topbar);
+    var isNew = true;
+    var topbar = $("div#topbar");
+    if(topbar.length == 0)
+    {
+        console.log("create topbar");
+        topbar = $("<div id='topbar'></div>");
+        topbar.css({
+        'position': 'fixed',
+        'right': '0px',
+        'top': '-160px',
+        'z-index': 9999,
+        'width': '100%',
+        'height': '160px',
+        'background-color': 'white'  // Confirm it shows up
+        });
+        $('body').append(topbar);
+        // topbar.append('<input id="mybutton" type="button" value="My button">');
+        // $("input#mybutton").click(clearTopBar());
+    }
+    else
+    {
+        console.log("use existing topbar");
+        topbar = topbar.first();
+        isNew = false;
+    }
 
   // animate the toolbar appreance
   topbar.animate(
@@ -38,54 +52,81 @@ function showTopBar() {
   500, 
   "linear", 
   function() {
-    console.log("animate complete");
+    autoHideBar();
   }); 
+
+  return isNew;
 }
 
 function showItemOnBar(item)
 {
     var topbar = $("div#topbar");
-    console.log(topbar);
-    topbar.append("<p>aaaaa</p>");
+    // console.log(topbar);
+    var string = '<span><a href="#"><img class="swan" width="100px" height="140px" id="'+ item.id +'" src="'+ item.images[0] +'" /></a></span>';
+    var imgDom = $(string);
+    imgDom.css({
+        'position' : 'relative',
+        'padding-top' : '10px',
+        'padding-left' : '10px',
+        'padding-right' : '10px'
+        });
+    topbar.append(imgDom);
+    // $("span a img.swan").click(clearTopBar());
 }
 
 function getItemListID(userid) {
 
-    showTopBar();
+    var isNew = showTopBar();
     var value = localStorage.getItem("item_list");
     var itemList = (value && JSON.parse(value));
     if(itemList && itemList.id)
     {
-        printItemListSummary(itemList);
-        console.log("already have an item list id " + itemList.id + ", sending data");
-        sendData(itemList);
+        // printItemListSummary(itemList);
+        if(isNew)
+        {
+            for(var i = 0; i < itemList.items.length; i++)
+            {
+                showItemOnBar(itemList.items[i]);
+            }
+            console.log("already have an item list id " + itemList.id + ", sending data");
+            sendData(itemList);
+        }
         return;
     }
-
-    $.ajax({
-    type : "POST",
-    url : "http://pavo-prototype.herokuapp.com/item_lists?t="+userid,
-    contentType: "application/json;charset=utf-8",
-    dataType : "json",
-    crossDomain: true,
-    success : function (msg) { 
-        console.log("create user with item_list_id " + msg.id);
-        var itemList = new Object();
-        itemList.id = msg.id;
+    else
+    {
+        itemList = new Object();
+        itemList.id = userid;
         itemList.items = [];
         localStorage.setItem("item_list", JSON.stringify(itemList));
-        console.log("saved item_list_id");
-        console.log(itemList);
         sendData(itemList);
-        // chrome.extension.sendMessage({ item_list_id : msg.id });
-    },
-    error: function(xhr, textStatus, error){
-      console.log(xhr.statusText);
-      console.log(textStatus);
-      console.log(error);
-  },
-    beforeSend: setHeader
-    });
+    }
+
+    // my toen is id dont use this anymore
+  //   $.ajax({
+  //   type : "POST",
+  //   url : "http://pavo-prototype.herokuapp.com/item_lists?t="+userid,
+  //   contentType: "application/json;charset=utf-8",
+  //   dataType : "json",
+  //   crossDomain: true,
+  //   success : function (msg) { 
+  //       console.log("create user with item_list_id " + msg.id);
+  //       var itemList = new Object();
+  //       itemList.id = msg.id;
+  //       itemList.items = [];
+  //       localStorage.setItem("item_list", JSON.stringify(itemList));
+  //       console.log("saved item_list_id");
+  //       // console.log(itemList);
+  //       sendData(itemList);
+  //       // chrome.extension.sendMessage({ item_list_id : msg.id });
+  //   },
+  //   error: function(xhr, textStatus, error){
+  //     // console.log(xhr.statusText);
+  //     // console.log(textStatus);
+  //     // console.log(error);
+  // },
+  //   beforeSend: setHeader
+  //   });
 }
 
 function sendData(itemList)
@@ -112,7 +153,7 @@ function sendData(itemList)
         var src = img.src;
         if(src.match(/(gif|png|jpg|jpeg)$/))
         {
-            console.log("found main image " + src);
+            // console.log("found main image " + src);
             srcList.unshift(src);
         }
     });
@@ -150,10 +191,10 @@ function sendData(itemList)
             itemList.items.push(item);
             console.log("saving item " + item.id);
             localStorage.setItem("item_list", JSON.stringify(itemList));
-            showItemOnBar();
+            showItemOnBar(item);
         },
         error: function(xhr, textStatus, error){
-          showItemOnBar();
+          // showItemOnBar(item);
           // console.log(xhr.statusText);
           // console.log(textStatus);
           // console.log(error);
@@ -162,4 +203,72 @@ function sendData(itemList)
         });
 }
 
-getItemListID("ninji");
+function deleteItemList(itemListID)
+{
+    localStorage.clear();
+    $.ajax({
+        type : "DELETE",
+        url : "http://pavo-prototype.herokuapp.com/item_lists/"+itemListID,
+        contentType: "application/json;charset=utf-8",
+        dataType : "json",
+        crossDomain: true,
+        success : function (msg) {
+            clearTopBar();
+        },
+        error: function(xhr, textStatus, error){
+          // showItemOnBar(item);
+          // console.log(xhr.statusText);
+          // console.log(textStatus);
+          // console.log(error);
+      },
+        beforeSend: setHeader
+        });
+}
+
+function clearTopBar()
+{
+    var topbar = $("div#topbar");
+    if(topbar.length > 0)
+    {
+        topbar.empty();
+    }
+}
+
+function autoHideBar()
+{
+    idleTime = 0;
+
+   //Increment the idle time counter every second.
+   var idleInterval = setInterval(timerIncrement, 1000);
+
+   function timerIncrement()
+   {
+     idleTime++;
+     if (idleTime > 3)
+     {
+       doPreload();
+     }
+   }
+
+   //Zero the idle timer on mouse movement.
+   var topbar = $("div#topbar");
+   topbar.mousemove(function(e){
+      idleTime = 0;
+   });
+
+   function doPreload()
+   {
+        topbar.animate(
+      {
+        top : "-160px"
+      }, 
+      500, 
+      "linear", 
+      function() {
+        topbar.remove();
+        console.log("leaving delete topbar");
+        clearInterval(idleInterval);
+      }); 
+         //Preload images, etc.
+       }
+}
